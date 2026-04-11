61ea9a4990007e6dcee7c521ad51589e5976fe20
taoki <84th.tccaoki@gmail.com>
2026-04-11 23:37:38 +0900

    fix: お知らせ一覧ページにデザインを追加

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -18,17 +18,35 @@
 		{#if data.news && data.news.length > 0}
 			{#each data.news as item}
 				<ul>
-					<li>
+					<li class="mx-auto mb-4 w-full list-none">
 						<a href="/news/{item.id}">
-							<p>{item.date}</p>
-							<p>{item.title}</p>
-							<p>{item.heading}</p>
+							<div
+								class="news-list flex border-l-2 pl-4"
+								style="border-left-color: var(--main-text-color);"
+							>
+								<div class="news-list-date-box mr-4">
+									<span class="news-list-month"
+										>{String(new Date(item.date).getMonth() + 1).padStart(2, '0')}</span
+									>
+									<span class="news-list-slash">/</span>
+									<span class="news-list-date"
+										>{String(new Date(item.date).getDate()).padStart(2, '0')}</span
+									>
+								</div>
+								<div class="truncate-parent flex-col">
+									<p class="truncate-title news-list-title ml-2 font-bold">{item.title}</p>
+									<p class="truncate-heading news-list-heading mb-2">{item.heading}</p>
+								</div>
+								<div class="news-list-icon my-auto ml-auto">
+									<i class="fa-solid fa-angles-right left-auto mr-2"></i>
+								</div>
+							</div>
 						</a>
 					</li>
 				</ul>
 			{/each}
 		{:else}
-			<p>お知らせの取得に失敗しました。</p>
+			<p>お知らせの取得に失敗しました</p>
 		{/if}
 	</section>
 </main>
